99af1f1c25016bb3b821a7aa81c0e9918e45bd5d
lola95520 <98558251+lola95520@users.noreply.github.com>
2022-02-08 18:34:43 +0100

    Update create_mariadb_admin_user.sh

--- srcs/requirements/mariadb/tools/create_mariadb_admin_user.sh
+++ srcs/requirements/mariadb/tools/create_mariadb_admin_user.sh
@@ -2,7 +2,6 @@ service mysql start
 
 mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -u root -e "CREATE DATABASE wordpress;"
-mysql -u root wordpress  < /wordpress.sql
 mysql -u root -e "USE wordpress; GRANT ALL PRIVILEGES ON * TO '${MYSQL_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
 mysql -u root -e "alter user 'root'@'localhost' identified by 'password'";
